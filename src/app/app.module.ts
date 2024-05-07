5c72eb835a20e1510f24604dbb83e88ecffbcfed
Eder Tassin <57120727+EderTassin@users.noreply.github.com>
2024-05-07 15:25:21 -0300

    save progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [	
     AppComponent,
       HomeComponent,
+      HeaderComponent,
+      FooterComponent,
       LoginComponent
    ],
   imports: [
@@ -21,7 +26,8 @@ import { LoginComponent } from './login/login.component';
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
