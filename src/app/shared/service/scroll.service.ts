100e47180dfbf473aef47f8c1b59d7f708beef51
Eder Tassin <57120727+EderTassin@users.noreply.github.com>
2024-07-24 19:31:13 -0300

    corrections

--- src/app/shared/service/scroll.service.ts
+++ src/app/shared/service/scroll.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
-import { environment } from 'src/environment/environment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
